6f893c284c624a8d46d5da7ab9340685805cc494
Roniere Rezende <roniere_rezende@yahoo.com.br>
2024-08-12 21:53:37 -0300

    Adding Machine Man Interface project developed in WRKits in course about Microcontrollers.

---
+++ STM32_MMI_WRKits/Core/Inc/MMI_WR.h
@@ -0,0 +1,24 @@
+/* ===================================================================================================
+
+	MMIWR v.01 MMIWR.h
+
+	Serial Interface with PCF8574
+	LCD 16x2 with backkight
+	Auxiliary LED
+	4 buttons
+
+	Compiler: STM32Cube IDE
+	MCU: STM32F411RET6
+	CLK: 100 MHZ
+
+	Author: Roniere Rezende
+	Date: 2024, July
+
+ =================================================================================================== */
+
+#ifndef SRC_MMI_WR_H_
+#define SRC_MMI_WR_H_
+
+
+
+#endif /* SRC_MMI_WR_H_ */
---
+++ STM32_MMI_WRKits/Core/Src/MMI_WR.c
@@ -0,0 +1,19 @@
+/* ===================================================================================================
+
+	MMIWR v.01 WWI_WR.c
+
+	Serial Interface with PCF8574
+	LCD 16x2 with backkight
+	Auxiliary LED
+	4 buttons
+
+	Compiler: STM32Cube IDE
+	MCU: STM32F411RET6
+	CLK: 100 MHZ
+
+	Author: Roniere Rezende
+	Date: 2024, July
+
+ =================================================================================================== */
+
+
